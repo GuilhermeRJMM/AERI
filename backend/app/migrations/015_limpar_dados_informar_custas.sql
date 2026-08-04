ca71710ab9d2e1076661bd0722e5d4c03fbcb89a
Guilherme Richard Jesus Morais Medeiros <guilherme.medeiros@cri.local>
2026-08-04 14:10:56 -0300

    chore: limpa dados atuais do Informar Custas

---
+++ backend/app/migrations/015_limpar_dados_informar_custas.sql
@@ -0,0 +1,4 @@
+-- Limpeza operacional solicitada em 04/08/2026.
+-- A estrutura, as permissões e a auditoria geral do AERI são preservadas.
+DELETE FROM eventos_custas_livro3_aeri;
+DELETE FROM custas_livro3_aeri;
